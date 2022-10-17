96b9eb8ed173854a3ba7192124610b3644fbbf71
Vaal <68valuta68@gmail.com>
2022-10-17 17:45:46 +0300

    Init commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val alphabetArray = mutableListOf<Char>()
+    val alphabet = "абвгдеёжзиклмнопрстуфхцчшщъыьэюя"
+    for (element in alphabet){
+        alphabetArray.add(element)
+
+    }
+    println(alphabetArray)
+}
